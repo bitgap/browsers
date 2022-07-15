01cfe57b78a9744036a8eed0dfdbd4a0ac82f3e6
bitgap <50538539+bitgap@users.noreply.github.com>
2022-07-15 14:14:02 +0000

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -11,7 +11,7 @@ rm google-chrome-stable_current_amd64.deb
 
 
 
-sudo apt install -y curl jq 
+sudo apt install -y curl jq openvpn
 
 version="$(curl -s https://aus1.torproject.org/torbrowser/update_3/release/downloads.json | jq -r ".version")"
 
