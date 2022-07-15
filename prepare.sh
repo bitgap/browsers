1411a89fe6c1975d072254fc689ff2b676a39e44
bitgap <50538539+bitgap@users.noreply.github.com>
2022-07-15 14:11:09 +0000

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt update
+
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+ 
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+ 
+rm google-chrome-stable_current_amd64.deb
+
+
+
+sudo apt install -y curl jq 
+
+version="$(curl -s https://aus1.torproject.org/torbrowser/update_3/release/downloads.json | jq -r ".version")"
+
+wget -O - "https://dist.torproject.org/torbrowser/"$version"/tor-browser-linux64-"$version"_en-US.tar.xz" | tar xvJ
