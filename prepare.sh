a8ba19ab874b7b357d9cbc21ecfa995ea5cd1260
bitgap <50538539+bitgap@users.noreply.github.com>
2022-12-20 13:44:38 +0000

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -21,7 +21,7 @@ sudo apt install -y curl jq openvpn
 
 version="$(curl -s https://aus1.torproject.org/torbrowser/update_3/release/downloads.json | jq -r ".version")"
 
-wget -O - "https://dist.torproject.org/torbrowser/"$version"/tor-browser-linux64-"$version"_en-US.tar.xz" | tar xvJ
+wget -O - "https://dist.torproject.org/torbrowser/"$version"/tor-browser-linux64-"$version"_ALL.tar.xz" | tar xvJ
 
 ./tor-browser_en-US/start-tor-browser.desktop
 
